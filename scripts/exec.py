8a61a46007f9e29958f224e4aca930eb7f8fb8f9
Andrei Lascu <andrei.lascu10@imperial.ac.uk>
2021-09-29 13:15:09 +0100

    Fixes for exec script

--- scripts/exec.py
+++ scripts/exec.py
@@ -2,6 +2,8 @@
 
 import argparse
 import os
+import shlex
+import subprocess
 import yaml
 
 ###############################################################################
@@ -9,11 +11,11 @@ import yaml
 ###############################################################################
 
 parser = argparse.ArgumentParser(description = "MF++ executor wrapper")
-parser.add_argument("mfreduce-bin", type=str,
+parser.add_argument("mfreduce_bin", type=str,
     help = "Path to MF++R binary")
-parser.add_argument("input-path", type=str,
+parser.add_argument("input_path", type=str,
     help = "Path to input test file to reduce")
-parser.add_argument("config-file-path", type=str,
+parser.add_argument("config_file_path", type=str,
     help = "Path to config file to use for reduction")
 parser.add_argument("--interest-script", type=str,
     help = "Path to interestingness test")
@@ -25,12 +27,12 @@ parser.add_argument("--output", type=str,
 ###############################################################################
 
 args = parser.parse_args()
-sep = os.path.sep()
+sep = os.path.sep
 
 if args.interest_script is None:
     interest_path = os.path.join(os.path.dirname(args.mfreduce_bin), "..", "scripts", "interestingness.py")
 if args.output is None:
-    output_path = os.path.join(os.path.dirname(args.input-path), "reduced.cpp")
+    output_path = os.path.join(os.path.dirname(args.input_path), "reduced.cpp")
 
 with open(args.config_file_path, 'r') as config_fd:
     config = yaml.load(config_fd, Loader=yaml.FullLoader)
@@ -39,9 +41,10 @@ cmake_path = config["cmake_path"]
 compile_commands_path = config["compile_commands_path"]
 compile_script = config["compile_script_path"]
 
-cmd = f"{args.mfreduce_bin} --output {output_path} --debug 2"
-      f"--p {compile_commands_path} --interest-test {interest_path}"
-      f"--cmake-path {cmake_path} --compile-script {compile_script}"
+cmd = f"{args.mfreduce_bin} --output {output_path} --debug 2 "\
+      f"--p {compile_commands_path} --interest-test {interest_path} "\
+      f"--cmake-path {cmake_path} --compile-script {compile_script} "\
+      f"{args.input_path}"
 
 print("Executing cmd " + cmd)
 reduce_proc = subprocess.Popen(shlex.split(cmd), encoding="utf-8")
